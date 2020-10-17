2d598b1a7910b0af8812cb2b9e0544ddf508bb88
Exyl <lyxe77@gmx.fr>
2020-10-17 22:57:55 +0200

    Added a custom control inheriting plot that displays tooltip on mouse hover

--- ViewModels/CommonViewModels/LevelGraphViewModel.cs
+++ ViewModels/CommonViewModels/LevelGraphViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using Whydoisuck.UIModel;
 
 namespace Whydoisuck.ViewModels.CommonViewModels
---
+++ Views/CommonControls/OxyPlotTooltipHover.xaml.cs
@@ -0,0 +1,36 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Whydoisuck.Views.CommonControls
+{
+    /// <summary>
+    /// Additional layer for Plot, so that tooltip is displayed on hover
+    /// </summary>
+    public partial class OxyPlotTooltipHover : OxyPlot.Wpf.Plot
+    {
+        private void InitController()
+        {
+            ActualController.UnbindMouseDown(OxyMouseButton.Left);
+            ActualController.BindMouseEnter(PlotCommands.HoverPointsOnlyTrack);
+        }
+
+        public OxyPlotTooltipHover()
+        {
+            InitializeComponent();
+            InitController();
+        }
+    }
+}
