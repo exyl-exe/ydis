14b264c4713fadcb871c540b3c921c09ef0409c9
Exyl <lyxe77@gmx.fr>
2020-11-23 21:26:26 +0100

    Added logic for window chrome

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Input;
 using Whydoisuck.DataSaving;
 using Whydoisuck.ViewModels;
 
---
+++ Views/WindowChromeCommands.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Whydoisuck.Views
+{
+    /// <summary>
+    /// Code-behind for WindowChromeCommands.xaml
+    /// </summary>
+    public partial class WindowChromeCommands : UserControl
+    {
+        public WindowState WindowState { get; private set; }
+
+        public WindowChromeCommands()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            AdjustWindowSize();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void AdjustWindowSize()
+        {
+            if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+        }
+    }
+}
