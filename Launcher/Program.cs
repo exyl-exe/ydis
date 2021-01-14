2839d7d59eb6424ea8a9aae487579e54e2a28486
Exyl <lyxe77@gmx.fr>
2021-01-14 19:38:29 +0100

    Added mutex for launcher

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -10,14 +10,19 @@ namespace WDISLauncher
 {
     public class Program
     {
-        const string processToDetect = "GeometryDash";
+        const string MUTEX_NAME = "WDISLauncherMutex";
+        const string PROCESS_TO_DETECT = "GeometryDash";
         readonly static string processToLaunch = WDISPathGetter.GetPath();
         const int scanRate = 3000;
+        private static Mutex _mutex;
+
         static void Main()
         {
+            _mutex = new Mutex(true, MUTEX_NAME, out var createdNew);
+            if (!createdNew) return;
             if (File.Exists(processToLaunch))
             {
-                var launcher = new Launcher(processToDetect, processToLaunch, scanRate);
+                var launcher = new Launcher(PROCESS_TO_DETECT, processToLaunch, scanRate);
                 launcher.Start();
             }
         }
