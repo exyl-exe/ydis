31210830afa8aea5619a270bf272e90b4e0f3ab7
Exyl <lyxe77@gmx.fr>
2021-01-14 15:19:16 +0100

    Introduced sub-project for launcher

---
+++ Launcher/Launcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Launcher
+{
+    public class Launcher
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
