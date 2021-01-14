9b1bbdd55ce79f9a23e2f0e099330101d654e99b
Exyl <lyxe77@gmx.fr>
2021-01-14 15:45:54 +0100

    Added method to check if an app was launched in the Detector class

--- Launcher/Detector.cs
+++ Launcher/Detector.cs
@@ -17,9 +17,17 @@ namespace Launcher
             this.applicationToWatchFor = processToWatchFor;
         }
 
+        /// <summary>
+        /// Checks if the app was launched
+        /// </summary>
+        /// <returns></returns>
         public bool AppWasLaunched()
         {
-            return false;
+            if (currentProcess != null && !currentProcess.HasExited) return false;
+            var matchingProcesses = Process.GetProcessesByName(applicationToWatchFor);
+            var appOpened = matchingProcesses.Length == 0;
+            currentProcess = appOpened ? matchingProcesses[0] : null;
+            return appOpened;
         }
     }
 }
